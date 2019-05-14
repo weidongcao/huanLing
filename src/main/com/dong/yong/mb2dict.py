983df05c1681c2b39ffba24706f02f343ae1f461
ubuntu <1774104802@qq.com>
2019-05-14 14:16:00 +0800

    小小输入法进行英文单词编码

---
+++ src/main/com/dong/yong/mb2dict.py
@@ -0,0 +1,79 @@
+'''
+五笔码表转字典
+'''
+
+
+def get_dict_from_mb(fps, dt):
+    with open(fps, 'r') as file:
+        lines = file.readlines()
+    for line in lines.__iter__():
+        kv = line.strip().split(' ')
+        if kv:
+            if kv[0] in dt:
+                dt[kv[0]].extend(kv[1:])
+            else:
+                dt[kv[0]] = kv[1:]
+    return dt
+
+
+def main():
+    dt = {}
+    dt = get_dict_from_mb('english_word_repeat_1.txt', dt)
+    dt = get_dict_from_mb('english_word_repeat_2.txt', dt)
+    dt = get_dict_from_mb('english_word_repeat_3.txt', dt)
+    dt = get_dict_from_mb('english_word_repeat_9.txt', dt)
+
+    cl1 = {}
+    cl2 = {}
+    cl3 = {}
+    cll = {}
+    for key, value in dt.items():
+        if value.__len__() == 1:
+            cl1[key] = value
+        elif value.__len__() == 2:
+            cl2[key] = value
+        elif value.__len__() == 3:
+            cl3[key] = value
+        elif value.__len__() >= 4:
+            cll[key] = value
+
+    print("没有重码的有: {} 个".format(cl1.__len__()))
+    print("重码的有: {} 个".format(dt.__len__() - cl1.__len__()))
+
+    print("重码率为2有的: {} 个".format(cl2.__len__()))
+    print("重码率为3有的: {} 个".format(cl3.__len__()))
+    print("重码率为4有的: {} 个".format(cll.__len__()))
+
+    content = ''
+    for key, value in dt.items():
+        content += key + ' ' + ' '.join(value) + '\n'
+
+    # with open('words.txt', 'w') as file:
+    #     file.write(content)
+
+    # i3 = 1
+    # for key, value in cl2.items():
+    #     print("index = {} --> key = {}, words = {}".format(i3, key, value))
+    #     i3 += 1
+    # i3 = 1
+    # for key, value in cl3.items():
+    #     print("index = {} --> key = {}, words = {}".format(i3, key, value))
+    #     i3 += 1
+    i3 = 1
+    for key, value in cll.items():
+        print("index = {} --> key = {}, words = {}".format(i3, key, value))
+        i3 += 1
+
+
+if __name__ == '__main__':
+    with open('words_6000.txt', 'r') as file:
+        lines = file.readlines()
+
+    words = []
+    for i in lines.__iter__():
+        word = lines[i].strip().lower()
+        words.append(word)
+    words = words.sort()
+    with open('words.txt', 'w') as file:
+        file.write("\n".join(words))
+
--- src/main/com/dong/yong/yong_english_words.py
+++ src/main/com/dong/yong/yong_english_words.py
@@ -1,16 +1,115 @@
 """
-把常用英文单词转为yong码表
+把常用英文单词进行五笔编码
 """
 
 import re
 
+fu = 'bcdfghjklmnpqrstvwxyz'
+yuan = 'aeiou'
+
+tuple3 = (
+    'contr',
+    'creat',
+    'comp',
+    'cons',
+    'cont',
+    'conv',
+    'stat',
+    'inst',
+    'legi'
+    'sens',
+    'expe',
+    'proc',
+    'com',
+    'con',
+    'the',
+    'pro',
+    'imp',
+    'ser',
+    'per',
+    'str',
+    'div',
+    'dve',
+    'off',
+    'ref',
+    'tra',
+    'ele',
+    'cha',
+    'res',
+    'sur',
+    'whe',
+    'pre',
+    'sup',
+    'sub',
+    'pri',
+    'gra',
+    'dis',
+    'des',
+    'dec',
+    'def',
+    'det',
+    'app',
+    'for',
+    'rep',
+    'ret',
+    'ass',
+    'pos',
+    'sho',
+    'sha',
+    'par',
+    'adv',
+    'acc',
+    'att',
+    'min',
+    'mon',
+    'sub',
+    'ex',
+    'in',
+    're',
+    'co',
+    'un')
+# tuple2 = ('ex', 'in', 're', 'co')
+combine = ('inter', 'trans', 'there', 'some', 'every', 'part', 'under', 'be', 'life', 'home', 'out', 'down', 'sh')
+
+
+# tuple2 = ()
+# supporter
+def get_wubi_key(value):
+    if value not in combine:
+        i = len(value) - 1
+        while i >= 2:
+            if value[0:i] in combine:
+                key = value[0:2] + value[i:i + 2]
+                return key
+            i -= 1
+
+    if len(value) >= 4:
+        i = len(value) - 1
+        while i >= 2:
+            if value[0:i] in tuple3:
+                key = value[0] + value[i:i + 2] + value[-1:]
+                return key
+            i -= 1
+        # if value.endswith('s'):
+        #     key = value[0:2] + value[-2:]
+        # elif value.endswith('es'):
+        #     key = value[0:2] + value[-3] + value[-1]
+        # else:
+        #     key = value[0:3] + value[-1]
+        key = value[0:3] + value[-1]
+    else:
+        return value
+
+    return key
+
+
+def test_key(word_list):
+    for i in range(word_list.__len__()):
+        print("单词:{} 五笔编码: {}".format(word_list[i], get_wubi_key(word_list[i])))
+
 
 def add_dict(dt, value):
-    if len(value) > 4:
-        key = value[0:3] + value[-1::]
-        # key = value[0:4]
-    else:
-        key = value
+    key = get_wubi_key(value)
 
     if key in dt:
         dt[key].append(value)
@@ -19,59 +118,102 @@ def add_dict(dt, value):
 
     return dt
 
-def get_words1():
-    pattern = re.compile('^\d+\s+(\w+)\s', re.S)
-    words = []
-    # with open("/home/wedo/Documents/common_english_words.txt", 'r') as file:
-    with open("/home/wedo/Documents/all_english_words.txt", 'r') as file:
-        lines = file.readlines()
 
-    print("总行数:{}".format(lines.__len__()))
-    for i in range(lines.__len__()):
-        line = lines[i]
-        ws = re.findall(pattern, line)
-        if ws.__len__() == 0:
-            print("没有匹配到 --> {}".format(line))
-        for word in iter(ws):
-            word = word.strip()
-            # print("第{}行:{}".format(i, word))
-            if word:
-                if not words.__contains__(word):
-                    words.append(word)
-                else:
-                    print("重复的单词 --> {}".format(word))
-            else:
-                print("空的单词 --> {}".format(word))
+def dict_to_wubi_code(dt, fps):
+    content = ''
+    for key, value in dt.items():
+        content += key + ' ' + ' '.join(value) + '\n'
 
-    # print(words)
-    # print("word count --> {}".format(words.__len__()))
+    with open(fps, 'w') as file:
+        file.write(content)
 
 
 def main():
-    with open("/home/wedo/Documents/words.txt", 'r') as file:
+    with open("words_6000.txt", 'r') as file:
         lines = file.readlines()
     words = []
+    word_rank = {}
+    repeat = 0
     for i in range(lines.__len__()):
-        words.append(lines[i].strip())
-    mb5 = {}
+        word = lines[i].strip()
+        if words.__contains__(word):
+            print("重复的单词:{}".format(word))
+            repeat += 1
+        else:
+            words.append(word)
+            word_rank[word] = i + 1
+    print("文本文件中重复的单词有 {} 个".format(repeat))
+    print("文本文件中单词一共有 {} 个".format(words.__len__()))
+
     mb4 = {}
     mb3 = {}
     mb2 = {}
+    cnt = {'wd5': 0, 'wd4': 0, 'wd3': 0, 'wd2': 0}
     for word in words:
         word = word.lower()
         if len(word) > 4:
-            mb5 = add_dict(mb5, word)
+            cnt['wd5'] += 1
+            mb4 = add_dict(mb4, word)
+
         elif len(word) == 4:
+            cnt['wd4'] += 1
             mb4 = add_dict(mb4, word)
         elif len(word) == 3:
+            cnt['wd3'] += 1
             mb3 = add_dict(mb3, word)
         else:
+            cnt['wd2'] += 1
             mb2 = add_dict(mb2, word)
 
-    print("{}个单词转为五笔码表的个数:{}".format(len(lines), len(mb5)))
-    # for key, value in mb5.items():
-    #     print("{} : {}".format(key, value))
+    print("3个字母以上有单词有: {}个".format(cnt['wd5'] + cnt['wd4']))
+    print("5个字母以上有单词有: {}个, 转换后五笔编码的个数:{}".format(cnt['wd5'], len(mb4)))
+    print("4个字母以上有单词有: {}个, 转换后五笔编码的个数:{}".format(cnt['wd4'], cnt['wd4']))
+    print("3个字母以上有单词有: {}个, 转换后五笔编码的个数:{}".format(cnt['wd3'], len(mb3)))
+    print("2个字母以上有单词有: {}个, 转换后五笔编码的个数:{}".format(cnt['wd2'], len(mb2)))
+
+    cl1 = {}
+    cl2 = {}
+    cl3 = {}
+    cll = {}
+    for key, value in mb4.items():
+        if value.__len__() == 1:
+            cl1[key] = value
+        elif value.__len__() == 2:
+            cl2[key] = value
+        elif value.__len__() == 3:
+            cl3[key] = value
+        elif value.__len__() >= 4:
+            cll[key] = value
+
+    print("没有重码的有: {} 个".format(cl1.__len__()))
+    print("重码的有: {} 个".format(mb4.__len__() - cl1.__len__()))
+
+    print("重码率为2有的: {} 个".format(cl2.__len__()))
+    print("重码率为3有的: {} 个".format(cl3.__len__()))
+    print("重码率为4有的: {} 个".format(cll.__len__()))
+
+    # dict_to_wubi_code(cl1, 'english_word_repeat_1.txt')
+    # dict_to_wubi_code(cl2, 'english_word_repeat_2.txt')
+    # dict_to_wubi_code(cl3, 'english_word_repeat_3.txt')
+    # dict_to_wubi_code(cll, 'english_word_repeat_9.txt')
+    i3 = 1
+    for key, value in cl2.items():
+        print("index = {} --> key = {}, words = {}".format(i3, key, value))
+        i3 += 1
+    i3 = 1
+    for key, value in cl3.items():
+        print("index = {} --> key = {}, words = {}".format(i3, key, value))
+        i3 += 1
+    i3 = 1
+    for key, value in cll.items():
+        print("index = {} --> key = {}, words = {}".format(i3, key, value))
+        i3 += 1
 
 
 if __name__ == '__main__':
     main()
+    test_key(
+        ['transmit', 'complain', 'completion', 'comparison', 'complication', 'composition', 'compulsion', 'companion',
+         'champion'])
+    print(get_wubi_key('supporter'))
+    print('presentation'[4:])
