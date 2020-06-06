88da2855d4aae0f249d2c62bfa85e78042727b21
lenovo-manjaro <1774104802@qq.com>
2020-06-06 20:37:44 +0800

    程序合并小小输入法个人词库

--- src/main/com/dong/yong/merge_user_file.py
+++ src/main/com/dong/yong/merge_user_file.py
@@ -7,8 +7,10 @@ import re
 
 import com.dong.yong.yong_util as util
 
+# 五笔源库
+wbx = 'wbx'
 # 汉字
-han_zi = 'han_zi'
+chinese = 'chinese'
 # 单词
 words = 'words'
 # 英文短语
@@ -22,7 +24,7 @@ gb18030 = 'gb18030'
 
 # mb是五笔个人码表的意思
 mb = dict()
-mb[han_zi] = dict()
+mb[chinese] = dict()
 mb[words] = dict()
 mb[phrase] = dict()
 mb[special] = dict()
@@ -36,11 +38,24 @@ if platform.system() == 'Windows':
 elif platform.system() == 'Linux':
     root_dir = r'/opt/workspace/github/myconf/yong'
 
+wbx_title = """name=五笔
+key=abcdefghijklmnopqrstuvwxyz
+len=4
+wildcard=z
+dwf=1
+auto_clear=4
+assist=z mb/pinyin.txt
+code_e2=p11+p12+p21+p22
+code_e3=p11+p21+p31+p32
+code_a4=p11+p21+p31+n11
+[DATA]
+"""
+
 # 需要被删除的编码
 dlist = []
 
 
-def get_dict_from_file(file_path):
+def get_dict_from_file(mb_name):
     """
     根据五笔码表文件生成对应的字典
     五笔码表文件中的一行:
@@ -49,29 +64,47 @@ def get_dict_from_file(file_path):
 
     生成的字典格式为:
     {"key1": ["词1",词2"], "key2":["词3","词4"]}
-    :param file_path:
+    :param mb_name:
     :param classify: 分类情况
     :return:
     """
-    if os.path.isfile(os.path.join(root_dir, file_path)):
+    if os.path.isfile(os.path.join(root_dir, mb_name)):
         dt = dict()
 
         # 将码表文件的所有行读出为一个列表
-        with open(os.path.join(root_dir, file_path), 'r', encoding=gb18030) as file:
+        with open(os.path.join(root_dir, mb_name), 'r', encoding=gb18030) as file:
             lines = file.readlines()
+        line_number = 0
+        if mb_name.endswith("wbx.txt"):
+            line_number = 11
 
-        for line in lines:
+        for index in range(line_number, len(lines)):
+            line = lines[index].strip()
+            if not line:
+                continue
             # 一行中的所有值以空格分隔
-            elist = line.strip().split(' ')
+            elist = line.split(' ')
+
+            # 去重
+            l = []
+            for e in elist[1:]:
+                if e not in l:
+                    l.append(e)
             # 判断key是否存在于字段中,如果存在追加,如果不存在添加
             if elist[0] not in dt:
-                dt[elist[0]] = elist[1:]
+                dt[elist[0]] = l
             else:
-                dt[elist[0]].extend(elist[1:])
-    return dt
+                dt[elist[0]].extend(l)
+        return dt
+    return None
 
 
 def check_all_str(s):
+    """
+    检查是否字母数字下划线
+    :param s:
+    :return:
+    """
     alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-'
     for c in s:
         if c not in alpha:
@@ -124,33 +157,30 @@ def add_user_to_mb(file_path):
             continue
         kv = line.strip().split(' ')
 
-        # 如果下标为-,表示这个编码需要被删除
-        if kv[0][1] == '-':
-            dlist.append(line)
-            continue
         # 下标
-        index = int(kv[0][1])
+        index = kv[0][1]
         # key
         key = kv[0][3:]
         # value
         value = kv[1]
         tup = (index, key, value)
 
-        flat = True
-        if check_str(value) == 'yong':
-            flat = add_item(mb[phrase], tup)
-        elif check_str(value) == 'zh':
-            flat = add_item(mb[han_zi], tup)
-        elif check_str(value) == 'en':
-            flat = add_item(mb[words], tup)
-        elif check_str(value) == 'maxture':
-            flat = add_item(mb[maxture], tup)
-        elif check_str(value) == 'special':
-            flat = add_item(mb[special], tup)
-
-        # 如果没有写入用户码表,说明需要写入主码表
-        if not flat:
+        if index == '-':
+            # 如果下标为-,表示这个编码需要被删除
             dlist.append(line)
+            del_item(mb, tup)
+        else:
+            # 如果下标不为-,表示这个编码为新增或者调换顺序
+            if check_str(value) == 'yong':
+                flat = add_item(mb[phrase], tup)
+            elif check_str(value) == 'zh':
+                flat = add_item(mb[chinese], tup)
+            elif check_str(value) == 'en':
+                flat = add_item(mb[words], tup)
+            elif check_str(value) == 'maxture':
+                flat = add_item(mb[maxture], tup)
+            elif check_str(value) == 'special':
+                flat = add_item(mb[special], tup)
 
 
 def add_item(mb, tup):
@@ -160,7 +190,7 @@ def add_item(mb, tup):
     :param tup: 类型为三元组,第一个为下标,第二个为key,第三个为value
     :return: 返回是否插入成功
     """
-    index = tup[0]
+    index = int(tup[0])
     key = tup[1]
     value = tup[2]
 
@@ -180,6 +210,24 @@ def add_item(mb, tup):
     return flat
 
 
+def del_item(mb, tup):
+    """
+    删除一条记录
+    :param mb:
+    :param tup:
+    :return:
+    """
+    key = tup[1]
+    value = tup[2]
+
+    for (mb_name, dt) in mb.items():
+        if key in dt.keys():
+            if value in dt[key]:
+                dt[key].remove(value)
+                print("删除记录成功, 词库: {}, {} --> {}".format(mb_name, key, value))
+                break
+
+
 def print_dict(dt, desc):
     print("-------------------------------------{}----------------------------------".format(desc))
     i3 = 1
@@ -190,30 +238,44 @@ def print_dict(dt, desc):
 
 def main():
     # 个人词库转字典
-    mb[han_zi]=get_dict_from_file('mb/my_hanzi.txt')
+    # 主码表
+    mb[wbx]=get_dict_from_file('mb/wbx.txt')
+    # 汉字
+    mb[chinese]=get_dict_from_file('mb/my_chinese.txt')
+    # 词组
     mb[phrase]=get_dict_from_file('mb/my_phrase.txt')
+    # 英语
     mb[words]=get_dict_from_file('mb/my_words.txt')
+    # 特殊字符
     mb[special]=get_dict_from_file('mb/my_special.txt')
+    #混合字符
     mb[maxture]=get_dict_from_file('mb/my_maxture.txt')
 
     #
     add_user_to_mb('user.txt')
     # add_user_mb('user.txt')
 
-    print_dict(mb[phrase], '英文短语')
-    print_dict(mb[words], "英文单词")
-    print_dict(mb[special], '特殊字符')
-    print_dict(mb[maxture], '中英混输')
-    print_dict(mb[han_zi], '汉字')
-    print('\n'.join(dlist))
+    # print_dict(mb[phrase], '英文短语')
+    # print_dict(mb[words], "英文单词")
+    # print_dict(mb[special], '特殊字符')
+    # print_dict(mb[maxture], '中英混输')
+    # print_dict(mb[chinese], '汉字')
+    # print('\n'.join(dlist))
 
-    util.dict_to_wubi_code(mb[han_zi], os.path.join(root_dir, 'mb/my_hanzi.txt'), gb18030)
+    util.dict_to_wubi_code(mb[chinese], os.path.join(root_dir, 'mb/my_chinese.txt'), gb18030)
     util.dict_to_wubi_code(mb[phrase], os.path.join(root_dir, 'mb/my_phrase.txt'), gb18030)
     util.dict_to_wubi_code(mb[words], os.path.join(root_dir, 'mb/my_words.txt'), gb18030)
     util.dict_to_wubi_code(mb[special], os.path.join(root_dir, 'mb/my_special.txt'), gb18030)
     util.dict_to_wubi_code(mb[maxture], os.path.join(root_dir, 'mb/my_maxture.txt'), gb18030)
-    with open(os.path.join(root_dir, 'main_mb.txt'), 'a', encoding=gb18030) as file:
-        file.write('\n'.join(dlist))
+
+    clist = []
+    for key, value in mb[wbx].items():
+        clist.append(key + ' ' + ' '.join(value))
+    clist.sort()
+    with open(os.path.join(root_dir, 'mb/wbx.txt'), 'w', encoding=gb18030) as file:
+        file.write(wbx_title)
+        file.write('\n'.join(clist))
+
     util.list_to_wubi_code(dlist, os.path.join(root_dir, 'delete_word.txt'), 'gb18030')
 
 
