ddc076401f62a8c5ad2a33690977e95b1fc7b577
i75930_win10 <1774104802@qq.com>
2020-08-15 23:59:05 +0800

    添加日志模块

--- src/main/com/dong/basical/compass_dir.py
+++ src/main/com/dong/basical/compass_dir.py
@@ -9,9 +9,13 @@
 """
 将小米摄像头生成的历史视频重构目录，8个小时一个文件夹，并进行压缩，为上传到百度云做准备
 """
+import sys
 import os
 import zipfile
 import shutil
+from logger import logger
+
+sys.path.append("logger")
 
 title = "摄像头视频-中牟老家后院-"
 
@@ -26,16 +30,18 @@ def get_zip_file(input_path, result):
 
 
 def zip_file_path(input_path, output_path, output_name):
-    f = zipfile.ZipFile(output_path + os.path.sep + output_name , 'w', zipfile.ZIP_DEFLATED)
-    filelist = []
-    get_zip_file(input_path, filelist)
-    for file in filelist:
-        f.write(file)
+    zip = zipfile.ZipFile(output_path + os.path.sep + output_name, 'w', zipfile.ZIP_DEFLATED)
+    for path, dirnames, filenames in os.walk(input_path):
+        fpath = path.replace(input_path, '')
+        for filename in filenames:
+            zip.write(os.path.join(path, filename), os.path.join(fpath, filename))
 
-    f.close()
+    zip.close()
 
 
 def main(file_path):
+    logger.info("test")
+
     root_path = os.path.abspath(os.path.dirname(file_path) + os.path.sep + ".")
     dir_list = os.listdir(file_path)
 
@@ -50,20 +56,19 @@ def main(file_path):
                 os.mkdir(new_dir_path)
 
             shutil.move(file_path + os.path.sep + dir_name, new_dir_path)
-    print("文件目录重构完成")
+    logger.info("文件目录重构完成")
     new_dir_list = os.listdir(root_path)
     for i in range(new_dir_list.__len__() - 1, -1, -1):
         dir_name = new_dir_list.pop(i)
         if dir_name.startswith(title):
             if not os.path.exists(root_path + os.path.sep + dir_name + ".zip"):
-                print("为文件夹 -> {} 创建压缩文件".format(dir_name))
+                logger.info("为文件夹 -> {} 创建压缩文件".format(dir_name))
                 zip_file_path(root_path + os.path.sep + dir_name, root_path, dir_name + ".zip")
-                print("压缩文件 {} 创建完成".format(dir_name + ".zip"))
-
-
+                logger.info("压缩文件 {} 创建完成".format(dir_name + ".zip"))
+            else:
+                logger.info("压缩文件: {}已经创建，跳过".format(dir_name + ".zip"))
 
 
 if __name__ == '__main__':
     main("F:\\摄像头\\摄像头视频-中牟老家后院\\2019MIJIA_RECORD_VIDEO")
     # main("F:\\摄像头\\test\\aaa")
-
---
+++ src/main/logger.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-08-15 22:38
+# @Author  : CaoWeidong
+# @Email   : caowd1990@163.com
+# @File    : logger.py
+# @Software: IntelliJ IDEA
+
+import logging.config
+
+import yaml
+
+__author__ = "caoweidong"
+
+
+class Logger(object):
+    def __init__(self):
+        # self.file = 'resources/logger.conf'
+        self.file = 'resources/config.yaml'
+
+    def get_logger(self, name="root"):
+        with open(self.file, 'r', encoding='utf-8') as f:
+            conf = yaml.load(f, Loader=yaml.FullLoader)
+            logging.config.dictConfig(conf)
+        # logging.config.fileConfig(self.file)
+        return logging.getLogger(name)
+
+
+logger = Logger().get_logger()
+logger.info("aaa")
