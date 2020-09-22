8ef060ac0bb8213c04188126d73f5903622ae32b
manjaro-home <caowd1990@gmail.com>
2020-09-22 20:56:47 +0800

    python --> update module logger configation

--- src/main/logger.py
+++ src/main/logger.py
@@ -8,24 +8,5 @@
 import logging.config
 import os
 
-import yaml
-
-__author__ = "caoweidong"
-
-
-class Logger(object):
-    def __init__(self):
-        # self.file = 'resources/logger.conf'
-        parent = os.path.dirname(os.path.realpath(__file__))
-        self.file = os.path.join(parent, 'resources/config.yaml')
-
-    def get_logger(self, name="root"):
-        with open( self.file, 'r', encoding='utf-8') as f:
-            conf = yaml.load(f, Loader=yaml.FullLoader)
-            logging.config.dictConfig(conf)
-        # logging.config.fileConfig(self.file)
-        return logging.getLogger(name)
-
-
-logger = Logger().get_logger()
-logger.info("aaa")
+logging.config.fileConfig(os.path.join(os.path.dirname(__file__), 'logger.ini'))
+logger = logging.getLogger("root")
